68ecbb06fb72ea1bfd0dde6091a0295f97f3a1b9
Bre 97 <by.sgatn3@Gmail.com>
2023-06-27 15:43:40 +0800

    build webpage

--- vite.config.js
+++ vite.config.js
@@ -2,31 +2,31 @@
 import { defineConfig } from "vite";
 
 export default defineConfig({
-  build: {
-    //打包文件目录
-    outDir: "es",
-    //压缩
-    //minify: false,
-    rollupOptions: {
-      //忽略打包vue文件
-      external: ["css","html","svg"],
-      input: ["./index.ts"],
-      output: [
-        {
-          //打包格式
-          format: "es",
-          //打包后文件名
-          entryFileNames: "[name].js",
-          //让打包目录和我们目录对应
-          preserveModules: true,
-          exports: "named",
-          //配置打包根目录
-          dir: "./switch",
-        },
-      ],
-    },
-    lib: {
-      entry: "./index.ts",
-    },
-  },
+  // build: {
+  //   //打包文件目录
+  //   outDir: "es",
+  //   //压缩
+  //   //minify: false,
+  //   rollupOptions: {
+  //     //忽略打包vue文件
+  //     external: ["css","html","svg"],
+  //     input: ["./index.ts"],
+  //     output: [
+  //       {
+  //         //打包格式
+  //         format: "es",
+  //         //打包后文件名
+  //         entryFileNames: "[name].js",
+  //         //让打包目录和我们目录对应
+  //         preserveModules: true,
+  //         exports: "named",
+  //         //配置打包根目录
+  //         dir: "./switch",
+  //       },
+  //     ],
+  //   },
+  //   lib: {
+  //     entry: "./index.ts",
+  //   },
+  // },
 });
