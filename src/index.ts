f3a3fe5f5130db0fcfb8e854c32e20cb7f032e79
Bre 97 <by.sgatn3@Gmail.com>
2023-07-08 12:18:50 +0800

    update Switch

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import { customElement, property } from 'lit/decorators.js'
 import './lib/Switch'
 import { LitElement, TemplateResult, html } from 'lit'
+import { Switch } from './lib/Switch'
 
 declare global {
   interface HTMLElementTagNameMap {
@@ -9,22 +10,5 @@ declare global {
 }
 
 @customElement('light-dark-switch')
-export class LightDarkSwitch extends LitElement {
-
-  @property({ type: Boolean }) md: boolean = false
-  @property({ type: Boolean }) selected: boolean = false;
-  @property({ type: Boolean }) disabled: boolean = false;
-  @property({ type: Boolean }) sync: boolean = false;
-
-  protected override render(): TemplateResult<1> {
-    if(this.md) {
-      return html`
-        <light-dark-switch-md ?selected="${this.selected}" ?disabled="${this.disabled}" ?sync="${this.sync}"></light-dark-switch-md>
-      `
-    }
-    return html`
-        <light-dark-switch-standard ?selected="${this.selected}" ?disabled="${this.disabled}" ?sync="${this.sync}"></light-dark-switch-standard>
-    
-    `
-  }
+export class LightDarkSwitch extends Switch {
 }
