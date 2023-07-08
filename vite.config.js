c36b6e0103f28ee9ed791d56e1a61eac969ccd37
Bre 97 <by.sgatn3@Gmail.com>
2023-07-08 11:32:01 +0800

    update config

--- vite.config.js
+++ vite.config.js
@@ -3,31 +3,23 @@ import { defineConfig } from "vite";
 
 export default defineConfig({
   base: '/light-dark-switch',
-  // build: {
-  //   //打包文件目录
-  //   outDir: "es",
-  //   //压缩
-  //   //minify: false,
-  //   rollupOptions: {
-  //     //忽略打包vue文件
-  //     external: ["css","html","svg"],
-  //     input: ["./index.ts"],
-  //     output: [
-  //       {
-  //         //打包格式
-  //         format: "es",
-  //         //打包后文件名
-  //         entryFileNames: "[name].js",
-  //         //让打包目录和我们目录对应
-  //         preserveModules: true,
-  //         exports: "named",
-  //         //配置打包根目录
-  //         dir: "./switch",
-  //       },
-  //     ],
-  //   },
-  //   lib: {
-  //     entry: "./index.ts",
-  //   },
-  // },
+  build: {
+    minify: false,
+    rollupOptions: {
+      external: ["css","html","svg"],
+      input: ["./src/index.ts"],
+      output: [
+        {
+          format: "es",
+          entryFileNames: "[name].js",
+          preserveModules: true,
+          exports: "named",
+          dir: "./switch",
+        },
+      ],
+    },
+    lib: {
+      entry: "./src/index.ts",
+    },
+  },
 });
