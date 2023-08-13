4c887d7b6b67d1812fc3c2e7079dc554f3ab247d
bre97-web <by.sgatn3@gmail.com>
2023-08-13 12:24:35 +0800

    fix: rename switch.ts

--- light-dark-switch/light-dark-switch.ts
+++ light-dark-switch/light-dark-switch.ts
@@ -1,12 +1,6 @@
 import { customElement } from 'lit/decorators.js'
 import { Switch } from './internal/switch'
 
-declare global {
-  interface HTMLElementTagNameMap {
-    'light-dark-switch': LightDarkSwitch
-  }
-}
-
 @customElement('light-dark-switch')
 export class LightDarkSwitch extends Switch {
 }
